4f28234f81816ec300ac065f39b75b7b67a3973c
kashibadzeakaki <41306000+kashibadzeakaki@users.noreply.github.com>
2018-07-17 00:28:06 +0400

    Update and rename newClient.py to client.py

--- client.py
+++ client.py
@@ -7,27 +7,29 @@ class TCPclient():
     def __init__(self,ip,port):
         self.ip=ip
         self.port=int(port)
+        #creating socket
         self.sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_address=(self.ip,self.port)
+        #connection to ip:port
         self.sock.connect(self.server_address)
     
 
     def work(self):
         try:
+            #while works until user press ctrl+c or write exit, after exit server will close connection 
             while True:
-                data=self.sock.recv(8192)
+                #getting data from server , max 2048 bytes 
+                data=self.sock.recv(2048)
                 if data == "":
                     exit()
                 print(data)
+                #waiting for user to enter next command name
                 req = raw_input()
+                #sending command name to server
                 self.sock.sendall(req)
         except socket.timeout:
             print("sock.close()")
             self.sock.close()
-
-
-
-
 
 if __name__ == '__main__':
     print("please enter ip address (localhost) ")
